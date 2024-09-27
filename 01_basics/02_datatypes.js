cb4690e8c8d5456f65abf44bd91332c9c11c619d
iam-arshadali <arshad.jgd@gmail.com>
2024-09-27 17:49:55 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //traet all js code as newer version
+
+let name = "Arshad"   //string
+let age  = 30       //num
+let isLoggedIn = true   //boolean
+
+// null = standalone value   Khaali
+// undefined = not defined or value is not assigned
+//symbol = unique 
+// object  = 
+
+console.table([typeof "Arshad", typeof 30, typeof isLoggedIn, typeof null]);
